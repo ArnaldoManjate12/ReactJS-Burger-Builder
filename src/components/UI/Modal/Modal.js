610672afe26ed4df98558afd9505e8f52b504d4d
ArnaldoManjate12 <arnaldo@rapidtrade.biz>
2020-04-14 20:45:20 +0200

    finished basic build

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,34 @@
-import React,{Fragment} from 'react';
+import React,{Fragment, Component} from 'react';
 import Backdrop from '../Backdrop/Backdrop';
 import './Modal.css';
 
 
-const modal = (props) => (
-    <Fragment>
-        <Backdrop  show={props.show}/>
-        <div className="Modal" style={{ 
-                                width : props.width ,
-                                height: props.height,
-                                transform: props.show ? 'translateY(0)' : 'translateY(-150vh)',
-                                opacity : props.show ? '1': '0'
-                            }}>
-        {props.children}
-        </div>
-    </Fragment>
+class Modal extends Component {
     
-);
+    shouldComponentUpdate( nextProps , nextState){
+        return this.props.show !== nextProps.show
+    }
 
-export default modal;
+    componentDidUpdate(){
+        console.log("The Component Updated");
+    }
+
+    render(){
+        return(
+            <Fragment>
+                <Backdrop  show={this.props.show}/>
+                <div className="Modal" style={{ 
+                                        width : this.props.width ,
+                                        height: this.props.height,
+                                        transform: this.props.show ? 'translateY(0)' : 'translateY(-150vh)',
+                                        opacity : this.props.show ? '1': '0'
+                                    }}>
+                {this.props.children}
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default Modal;
+    
